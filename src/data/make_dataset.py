7aa3e37ef29fc78577465e8e7d25e90e98e88ac9
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2024-06-06 15:28:02 +0700

    Refactor: Change path dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,18 +3,18 @@ from glob import glob
 # --------------------------------------------------------------
 # Read single CSV file
 # --------------------------------------------------------------
-single_read_acc=pd.read_csv("../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv")
-single_read_gyr=pd.read_csv("../../data/raw/MetaMotion/A-bench-heavy_MetaWear_2019-01-14T14.22.49.165_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv")
-# --------------------------------------------------------------
+single_read_acc=pd.read_csv("https://github.com/atrkhomeini/Machine-Learning-Ops-Counts-Repitition/raw/experiment/data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv")
+single_read_gyr=pd.read_csv("https://github.com/atrkhomeini/Machine-Learning-Ops-Counts-Repitition/raw/experiment/data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv")
+# --------------------------------------------------------------# --------------------------------------------------------------
 # List all data in data/raw/MetaMotion
 # --------------------------------------------------------------
-file=glob("../../data/raw/MetaMotion/*.csv")
+file=glob("https://github.com/atrkhomeini/Machine-Learning-Ops-Counts-Repitition/raw/experiment/data/raw/MetaMotion/*.csv")
 len(file)
 # --------------------------------------------------------------
 # Extract features from filename
 # --------------------------------------------------------------
-data_path="../../data/raw/MetaMotion/*.csv"
-f=file[0]
+data_path="https://github.com/atrkhomeini/Machine-Learning-Ops-Counts-Repitition/raw/experiment/data/raw/MetaMotion/*.csv"
+f=file[-1]
 
 participants=f.split("-")[0].replace("../../data/raw/MetaMotion\\","")
 label=f.split("-")[1]
